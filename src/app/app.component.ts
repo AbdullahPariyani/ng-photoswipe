2d15bbcf142a5f4f7fa784cd6859f0cfa66039cb
Abdullah Pariyani <abdullah.mscit15@gmail.com>
2024-02-23 21:32:05 +0300

    PhotoSwipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, NgZone } from '@angular/core';
+
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-photoswipe';
+export class AppComponent implements AfterContentInit {
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterContentInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      const lightbox = new PhotoSwipeLightbox({
+        gallery: '#gallery--getting-started',
+        children: 'a',
+        pswpModule: PhotoSwipe,
+      });
+
+      lightbox.init();
+    });
+  }
 }
